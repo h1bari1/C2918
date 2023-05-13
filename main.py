f6036dac4b64b76216e69075ce050e4e555c5626
h1bari <vhibari1@gmail.com>
2023-05-13 18:56:45 +0300

    sims

--- main.py
+++ main.py
@@ -68,6 +68,83 @@ class Human:
             self.satiety += 5
             self.home.food -= 5
 
+    def work(self):
+        if self.car.drive():
+            pass
+        else:
+            if self.car.fuel < 20:
+                self.shopping("fuel")
+                return
+            else:
+                self.to_repair()
+                return
+        self.money += self.job.salary
+        self.gladness -= self.job.gladness_less
+        self.satiety -= 4
+
+    def shopping(self, manage):
+        if self.car.drive():
+            pass
+        else:
+            if self.car.fuel < 20:
+                manage = "fuel"
+            else:
+                self.to_repair()
+                return
+
+        if manage == 'fuel':
+            print("I bought fuel")
+            self.money -= 100
+            self.car.fuel += 100
+        elif manage == 'food':
+            print("I bought food")
+            self.money -= 50
+            self.home.food += 50
+        elif manage == 'delicacies':
+            print("Yay! Delicious!")
+            self.gladness += 10
+            self.satiety += 2
+            self.money -= 15
+
+    def chill(self):
+        self.gladness += 10
+        self.home.mess += 5
+
+    def clean_home(self):
+        self.gladness -= 5
+        self.home.mess += 0
+
+    def to_repair(self):
+        self.car.strength += 100
+        self.money -= 50
+
+    def days_indexes(self,day):
+        day = f"Today {day} of {self.name}'s life"
+        print(f"{day:^50}","\n")
+        human_indexes = self.name + "`s indexes"
+        print(f"{human_indexes:^50}","\n")
+        print(f"Money - {self.money}")
+        print(f"Satiety - {self.satiety}")
+        print(f"Gladness - {self.gladness}")
+        home_indexes = "Home indexes"
+        print(f"{home_indexes:^50}","\n")
+        print(f"Food - {self.home.food}")
+        print(f"Mess - {self.home.mess}")
+        car_indexes = f"{self.car.brand} car indexes"
+        print(f"{car_indexes:^50}", "\n")
+        print(f"Fuel - {self.car.fuel}")
+        print(f"Strength - {self.car.strength}")
+
+    def is_alive(self):
+        if self.gladness < 0:
+            print("Depression...")
+            return False
+        if self.satiety < 0:
+            print("Dead..")
+            return False
+        if self.money <= 500:
+            print("Bankrupt...")
+            return False
 
 class Auto:
     pass
