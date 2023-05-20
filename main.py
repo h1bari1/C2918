399f298003d55b5d5125de242e813d6fb59e29cc
h1bari <vhibari1@gmail.com>
2023-05-20 19:03:21 +0300

    test

---
+++ main.py
@@ -0,0 +1,38 @@
+class Hello:
+    def __init__(self):
+        print("Hello")
+
+
+class HelloWorld(Hello):
+    def __init__(self):
+        super().__init__()
+        print("World!")
+
+
+hello_world = HelloWorld()
+
+
+class Computer:
+    def __init__(self):
+        super().__init__()
+        self.memory = 128
+
+
+class Display:
+    def __init__(self):
+        super().__init__()
+        self.resolution = "4k"
+
+    def test(self):
+        print('test')
+
+
+class SmartPhone(Display, Computer):
+    def print_info(self):
+        super().test()
+        print(self.resolution)
+        print(self.memory)
+
+
+iphone = SmartPhone()
+iphone.print_info()
